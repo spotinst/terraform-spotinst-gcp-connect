b08c457b407c7812024b1ae4c0a2586761e72d35
Steven Feltner <steven.feltner@netapp.com>
2022-01-24 12:23:10 -0800

    Add sensitive output

--- outputs.tf
+++ outputs.tf
@@ -6,4 +6,5 @@ output "spot_account_id" {
 output "private_key" {
     description = "private key for service account"
     value = google_service_account_key.key.private_key
+    sensitive = true
 }

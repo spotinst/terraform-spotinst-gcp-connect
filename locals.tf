56749e540fb0e62a296e0372419a74c1b6aa8840
Steven Feltner <steven.feltner@netapp.com>
2022-01-24 20:56:41 -0800

    remove unsensetive

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   cmd = "${path.module}/scripts/spot-account"
   account_id = data.external.account.result["account_id"]
-  spotinst_token = nonsensitive(var.spotinst_token)
+  spotinst_token = var.spotinst_token
   private_key = nonsensitive(google_service_account_key.key.private_key)
 }
 

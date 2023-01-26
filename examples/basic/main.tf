913b77c7abaf70c013cf893f6fd1221c3b7071be
Steven Feltner <steven.feltner@netapp.com>
2023-01-25 22:56:32 -0800

    Format and update examples

---
+++ examples/basic/main.tf
@@ -0,0 +1,10 @@
+#Call the module to create a Spot account and link a project
+module "spot-gcp-connect" {
+  source         = "spotinst/gcp-connect/spotinst"
+  spotinst_token = "Redacted"
+  project        = "project1"
+}
+
+output "spot_account_id" {
+  value = module.spot-gcp-connect.spot_account_id
+}

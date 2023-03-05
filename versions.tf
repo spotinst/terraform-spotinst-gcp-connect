18a1ff6501f783cde82e18dd105f2057bbf8d2ea
Steven Feltner <steven.feltner@netapp.com>
2023-03-04 19:08:07 -0800

    Update versions

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">=0.14.0"
+  required_version = ">= 0.14.0"
   required_providers {
     google = {
-      version = ">= v4.0.0"
+      version = ">= 4.0.0"
     }
   }
 }
